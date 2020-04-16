2ed85548fc00212ab86c1dc9a2550db13a098204
Chimereze Alfred <ChimerezeAlfred@Chimerezes-MacBook-Pro.local>
2020-04-16 14:35:00 +0100

    cors headers

--- server/server.js
+++ server/server.js
@@ -44,6 +44,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 //Adding routes
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", userRoutes);
